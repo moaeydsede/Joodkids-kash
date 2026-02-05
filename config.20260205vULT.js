3574a8afced256267336c8c4f26cf42b4a742560
moaeydsede <moaeydsede98@gmail.com>
2026-02-05 10:12:31 +0200

    Update API_BASE URL in configuration file

--- config.20260205vULT.js
+++ config.20260205vULT.js
@@ -2,7 +2,7 @@
 window.CONFIG = {
   APP_TITLE: "Cash & Wallet Manager VIP",
   // ضع هنا رابط Apps Script exec:
-  API_BASE: "PUT_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
-  AUTH: { ENABLED: true },
+  API_BASE: https: "//script.google.com/macros/s/AKfycbxjLTaRgUK3aJpfm_Z2d0oM1D9Es6bsFsR4VKDrj5xrX1yOW66npu0uMC5GDxlqGGYq/exec
+  AUTH: { ENABLED: true },"
   SESSION_KEY: "wm_session_20260205vULT",
 };
